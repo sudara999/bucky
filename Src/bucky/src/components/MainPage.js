2b8651129cbb7cbdea97e027889c11a9d7890ca1
Sudara Ranasinghe <sudara1999@gmail.com>
2019-12-09 04:34:14 +0900

    Fixed sidebar css

--- Src/bucky/src/components/MainPage.js
+++ Src/bucky/src/components/MainPage.js
@@ -1,55 +1,22 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
-import mainStyles from './MainPage.module.css';
+import styles from './MainPage.module.css';
 import cx from 'classnames';
 import SideBar from "./SideBar";
 class MainPage extends Component {
 
-    componentDidMount() {
-
-    }
-
-    state = {
-        isActive: true,
-    }
-
-    changeActive = () => {
-        this.setState({ isActive: !this.state.isActive });
-    }
-
     render() {
         return (
             <div>
-                <div className={mainStyles["wrapper"]}>
-
-                    <div>
-                        <Map
-                            google={this.props.google}
-                            zoom={8}
-                            style={mapStyles}
-                            initialCenter={{ lat: 47.444, lng: -122.176 }}
-                        />
-                        <div className={mainStyles.fixed}>
-                            <div id={mainStyles["content"]} className={mainStyles.onTop}>
-                            <SideBar active={this.state.isActive} />
-                                {/* Button */}
-                                <nav className={cx(mainStyles.navbar, mainStyles["navbar-expand-lg"], mainStyles["navbar-light"], mainStyles["bg-light"])}>
-                                    <div className={mainStyles["container-fluid"]}>
-                                        <button type="button" id={mainStyles["sidebarCollapse"]} className={this.state.isActive ? mainStyles["active"] : mainStyles["inactive"]} onClick={this.changeActive}>
-                                            <span></span>
-                                            <span></span>
-                                            <span></span>
-                                        </button>
-                                    </div>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+                <Map
+                    google={this.props.google}
+                    zoom={8}
+                    style={mapStyles}
+                    initialCenter={{ lat: 47.444, lng: -122.176 }}
+                />
+                <SideBar />
             </div>
-        )
+        );
     }
 }
 
--- Src/bucky/src/components/SideBar.js
+++ Src/bucky/src/components/SideBar.js
@@ -4,53 +4,68 @@ import styles from "./SideBar.module.css";
 
 class SideBar extends Component {
 
+    state = {
+        isActive: false,
+    }
+
+    changeActive = () => {
+        this.setState({ isActive: !this.state.isActive });
+    }
+
     render() {
         return (
-            <nav id={styles["sidebar"]} className={this.props.active ? styles["active"] : styles["inactive"]}>
-                <div className={styles["sidebar-header"]}>
-                    <h3>Bucky</h3>
-                </div>
-                <ul className={cx(styles["list-unstyled"], styles["components"])}>
-                    <p>Dummy Heading</p>
-                    <li className={styles["active"]}>
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className={cx(styles.l, styles["dropdown-toggle"])}>Home</a>
-                        <ul className={cx(styles.collapse, styles["list-unstyled"])} id="homeSubmenu">
-                            <li>
-                                <a className={styles.l} href="#">Dummy Div 1</a>
-                            </li>
-                            <li>
-                                <a className={styles.l} href="#">Dummy Div 2</a>
-                            </li>
-                            <li>
-                                <a className={styles.l} href="#">Dummy Div 3</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a className={styles.l} href="#">Description</a>
-                    </li>
-                    <li>
-                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className={cx(styles.l, styles["dropdown-toggle"])}>Pages</a>
-                        <ul className={cx(styles.collapse, styles["list-unstyled"])} id="pageSubmenu">
-                            <li>
-                                <a className={styles.l} href="#">Possibly more stuff</a>
-                            </li>
-                            <li>
-                                <a className={styles.l} href="#">Possibly even more stuff</a>
-                            </li>
-                            <li>
-                                <a className={styles.l} href="#">Yeap, you got it. More stuff</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a className={styles.l} href="#">Saved Places</a>
-                    </li>
-                    <li>
-                        <a className={styles.l} href="#">Your profile</a>
-                    </li>
-                </ul>
-            </nav>
+            <div className={styles.wrapper}>
+                <nav id={styles["sidebar"]} className={this.state.isActive ? styles["active"] : styles["inactive"]}>
+                    <div className={styles["sidebar-header"]}>
+                        <h3>Bucky</h3>
+                    </div>
+                    <ul className={cx(styles["list-unstyled"], styles["components"])}>
+                        <p>Dummy Heading</p>
+                        <li className={styles["active"]}>
+                            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className={cx(styles.l, styles["dropdown-toggle"])}>Home</a>
+                            <ul className={cx(styles.collapse, styles["list-unstyled"])} id="homeSubmenu">
+                                <li>
+                                    <a className={styles.l} href="#">Dummy Div 1</a>
+                                </li>
+                                <li>
+                                    <a className={styles.l} href="#">Dummy Div 2</a>
+                                </li>
+                                <li>
+                                    <a className={styles.l} href="#">Dummy Div 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a className={styles.l} href="#">Description</a>
+                        </li>
+                        <li>
+                            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className={cx(styles.l, styles["dropdown-toggle"])}>Pages</a>
+                            <ul className={cx(styles.collapse, styles["list-unstyled"])} id="pageSubmenu">
+                                <li>
+                                    <a className={styles.l} href="#">Possibly more stuff</a>
+                                </li>
+                                <li>
+                                    <a className={styles.l} href="#">Possibly even more stuff</a>
+                                </li>
+                                <li>
+                                    <a className={styles.l} href="#">Yeap, you got it. More stuff</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a className={styles.l} href="#">Saved Places</a>
+                        </li>
+                        <li>
+                            <a className={styles.l} href="#">Your profile</a>
+                        </li>
+                    </ul>
+                </nav>
+                <button type="button" id={styles["sidebarCollapse"]} className={this.state.isActive ? styles["active"] : styles["inactive"]} onClick={this.changeActive}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
         );
     }
 }
